f76d610760b493381f8a98e9b88ed0d4876aa03e
fthomas <thomas.foltzer@ynov.com>
2023-11-18 12:28:58 +0100

    fix: totues les images sont mtn différentes

--- App/views/favoriteView.php
+++ App/views/favoriteView.php
@@ -9,7 +9,7 @@
     {% for annonce in annonces %}
     <a href="/detailsLogement?id={{ annonce.annonceID }}" id="lien-annonce">
         <div class="annonce">
-            <img src="https://a2.muscache.com/im/pictures/6152848/b04eddeb_original.jpg?aki_policy=x_medium">
+            <img src="{{ annonce.image }}" id="img-annonce-home">
             <div class="zone-prix">{{annonce.price}} €/nuit</div>
             <div class="description">
                 <h4>{{annonce.name}}</h4>
