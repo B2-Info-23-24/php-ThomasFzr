613209fd39c1396225146876e7f809d7a3c29823
fthomas <thomas.foltzer@ynov.com>
2023-11-18 16:15:51 +0100

    ajout reservation controller et redirection vers connexion si pas connecté

---
+++ App/controllers/ReservationController.php
@@ -0,0 +1,21 @@
+<?php
+class ReservationController
+{
+
+
+    private $twig;
+
+    public function __construct($twig)
+    {
+        $this->twig = $twig;
+    }
+
+    function getReservation()
+    {
+        if (isset($_SESSION['userID'])) {
+            echo $this->twig->render('reservationView.php');
+        } else {
+            header('Location: /connection');
+        }
+    }
+}
