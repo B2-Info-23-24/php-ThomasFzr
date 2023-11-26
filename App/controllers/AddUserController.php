a288acc44c5ae6aed77b717d3712aa24ad65820c
fthomas <thomas.foltzer@ynov.com>
2023-11-26 15:24:44 +0100

    fix: addAnnonce et addUser pas accesible (url) si pas connecté en tant qu'admin

--- App/controllers/AddUserController.php
+++ App/controllers/AddUserController.php
@@ -3,33 +3,37 @@ class AddUserController
 {
     public function addUser()
     {
-        require_once __DIR__ . '/../models/Database.php';
-        $db = new Database();
+        if (isset($_SESSION['isAdmin'])) {
+            require_once __DIR__ . '/../models/Database.php';
+            $db = new Database();
 
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $mail = $_POST["mail"];
-            $pwd = $_POST["pwd"];
-            $isAdmin = $_POST["isAdmin"];
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $mail = $_POST["mail"];
+                $pwd = $_POST["pwd"];
+                $isAdmin = $_POST["isAdmin"];
 
-            $name = null;
-            $surname = null;
-            $phoneNbr = null;
+                $name = null;
+                $surname = null;
+                $phoneNbr = null;
 
-            if (isset($_POST["name"])) {
-                $name = $_POST["name"];
-            }
-            if (isset($_POST["surname"])) {
-                $surname = $_POST["surname"];
-            }
-            if (isset($_POST["phoneNbr"])) {
-                $phoneNbr = $_POST["phoneNbr"];
-            }
+                if (isset($_POST["name"])) {
+                    $name = $_POST["name"];
+                }
+                if (isset($_POST["surname"])) {
+                    $surname = $_POST["surname"];
+                }
+                if (isset($_POST["phoneNbr"])) {
+                    $phoneNbr = $_POST["phoneNbr"];
+                }
 
-            if ($db->addUser($mail, $pwd, $isAdmin, $name, $surname, $phoneNbr)) {
-                header('Location: /detailsUtilisateur');
-            }else{
-                header('Location: /detailsUtilisateur');
+                if ($db->addUser($mail, $pwd, $isAdmin, $name, $surname, $phoneNbr)) {
+                    header('Location: /detailsUtilisateur');
+                } else {
+                    header('Location: /detailsUtilisateur');
+                }
             }
+        } else {
+            header('Location: /');
         }
     }
 }
