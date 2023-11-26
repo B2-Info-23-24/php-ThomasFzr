909c2ee72fa5708147c8eb714671f7a377b28373
fthomas <thomas.foltzer@ynov.com>
2023-11-26 17:29:10 +0100

    ajout: controle si connecté ou non avant de pouvoir ajouter un avis, edit les infos du compte

--- App/controllers/EditInfoUserController.php
+++ App/controllers/EditInfoUserController.php
@@ -3,38 +3,42 @@ class EditInfoUserController
 {
     public function processEditInfoUser()
     {
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            require_once __DIR__ . '/../models/Database.php';
-            $database = new Database();
-            $successMsg = "";
+        if (isset($_SESSION['userID'])) {
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                require_once __DIR__ . '/../models/Database.php';
+                $database = new Database();
+                $successMsg = "";
 
-            if (isset($_POST["name"]) && $_POST["name"] != '') {
-                $database->updateTable("User", "name", $_POST["name"], $_SESSION["mail"]);
-                $successMsg = "Nom";
-            }
-            if (isset($_POST["surname"]) && $_POST["surname"] != '') {
-                $database->updateTable("User", "surname", $_POST["surname"], $_SESSION["mail"]);
-                $successMsg = $successMsg . " Prénom";
-                $_SESSION['surname'] = $_POST["surname"];
-            }
-            if (isset($_POST["phoneNbr"]) && $_POST["phoneNbr"] != '') {
-                $database->updateTable("User", "phoneNbr", $_POST["phoneNbr"], $_SESSION["mail"]);
-                $successMsg = $successMsg . " Numéro de téléphone";
-            }
-            if (isset($_POST["mail"]) && $_POST["mail"] != '') {
-                $database->updateTable("User", "mail", $_POST["mail"], $_SESSION["mail"]);
-                $_SESSION["mail"] = $_POST["mail"];
-                $successMsg = $successMsg . " Mail";
-            }
-            if ((isset($_POST["name"]) && $_POST["name"] != '') || (isset($_POST["surname"]) && $_POST["surname"] != '')
-                || (isset($_POST["phoneNbr"]) && $_POST["phoneNbr"] != '') || (isset($_POST["mail"]) && $_POST["mail"] != '')
-            ) {
-                $_SESSION['successMsg'] = $successMsg . " changé avec succès";
-            }
+                if (isset($_POST["name"]) && $_POST["name"] != '') {
+                    $database->updateTable("User", "name", $_POST["name"], $_SESSION["mail"]);
+                    $successMsg = "Nom";
+                }
+                if (isset($_POST["surname"]) && $_POST["surname"] != '') {
+                    $database->updateTable("User", "surname", $_POST["surname"], $_SESSION["mail"]);
+                    $successMsg = $successMsg . " Prénom";
+                    $_SESSION['surname'] = $_POST["surname"];
+                }
+                if (isset($_POST["phoneNbr"]) && $_POST["phoneNbr"] != '') {
+                    $database->updateTable("User", "phoneNbr", $_POST["phoneNbr"], $_SESSION["mail"]);
+                    $successMsg = $successMsg . " Numéro de téléphone";
+                }
+                if (isset($_POST["mail"]) && $_POST["mail"] != '') {
+                    $database->updateTable("User", "mail", $_POST["mail"], $_SESSION["mail"]);
+                    $_SESSION["mail"] = $_POST["mail"];
+                    $successMsg = $successMsg . " Mail";
+                }
+                if ((isset($_POST["name"]) && $_POST["name"] != '') || (isset($_POST["surname"]) && $_POST["surname"] != '')
+                    || (isset($_POST["phoneNbr"]) && $_POST["phoneNbr"] != '') || (isset($_POST["mail"]) && $_POST["mail"] != '')
+                ) {
+                    $_SESSION['successMsg'] = $successMsg . " changé avec succès";
+                }
 
-            header('Location: /detailsCompte');
+                header('Location: /detailsCompte');
+            } else {
+                echo "erreur edit info user";
+            }
         } else {
-            echo "erreur edit info user";
+            header('Location: /connection');
         }
     }
 }
--- App/controllers/ProcessAvisController.php
+++ App/controllers/ProcessAvisController.php
@@ -5,15 +5,19 @@ class ProcessAvisController
 
     public function insertAvis($annonceID)
     {
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            require_once __DIR__ . '/../models/Database.php';
-            $db = new Database();
+        if (isset($_SESSION['userID'])) {
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                require_once __DIR__ . '/../models/Database.php';
+                $db = new Database();
 
-            $date = new DateTime('now');
-            $date = $date->format('Y-m-d');
+                $date = new DateTime('now');
+                $date = $date->format('Y-m-d');
 
-            $db->insertReview($annonceID,  $_POST["grade"], $_POST["comment"], $date);
-            header("Location: /detailsLogement/$annonceID");
+                $db->insertReview($annonceID,  $_POST["grade"], $_POST["comment"], $date);
+                header("Location: /detailsLogement/$annonceID");
+            }
+        } else {
+            header('Location: /connection');
         }
     }
 }
